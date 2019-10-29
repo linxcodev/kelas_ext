d7b9c5ef695e8176678554d5759f5ea4522dca69
linxcodev <fajrul452@gmail.com>
2019-10-29 17:00:01 +0700

    event ayat

--- assets/js/newtab.js
+++ assets/js/newtab.js
@@ -60,10 +60,23 @@ function ayat(res) {
   $('terjemah').innerHTML = res.ayat.data.id[0].teks
 }
 
+function change(surahSelect, ayatSelect) {
+  get("https://api.banghasan.com/quran/format/json/surat/" + surahSelect + "/ayat/" + ayatSelect, ayat)
+}
+
 window.onload = function () {
+  surahSelected = 1
   // event surah
   $('listSurah').addEventListener('change', function () {
-    get("https://api.banghasan.com/quran/format/json/surat/"+this.value, listAyat)
+    surahSelected = this.value
+    change(surahSelected, 1)
+    get("https://api.banghasan.com/quran/format/json/surat/"+surahSelected, listAyat)
+  })
+
+  // event ayat
+  $('listAyat').addEventListener('change', function () {
+    change(surahSelected, this.value)
+    // get("https://api.banghasan.com/quran/format/json/surat/" + surahSelected + "/ayat/" + this.value, ayat)
   })
 
   get("https://api.banghasan.com/quran/format/json/surat/1/ayat/1", ayat)
